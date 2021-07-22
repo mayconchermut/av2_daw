78540fada075da0c9885c2de4370d2b1cf4903d2
mayconchermut <mayconchermut@gmail.com>
2021-07-22 02:45:38 -0300

    quase finalizado

--- buscar.php
+++ buscar.php
@@ -18,7 +18,7 @@
             xhr.send();
             xhr.onreadystatechange = function(){
                 if (this.readyState = 4 && this.status == 200){
-                    var data = JSON.parse(this.responseText);
+                    var data = JSON.parse(this?.responseText || "{}");
                     var html ="";
                     var html2 ="";
                     
@@ -30,6 +30,11 @@
                       var estoque = data[a].quantidade;
                       var ativo = data[a].ativo;
                       var ativovalido;
+                      var fabricante = data[a].fabricante;
+                      var tipoprod = data[a].tipoprod;
+                      var desc = data[a].descricao;
+                      var dataform = data[a].data;
+                      var peso = data[a].peso;
                       
                       if(ativo == 1){
                         var ativovalido = 'Ativo';
@@ -50,7 +55,7 @@
                               <td>` +estoque+ `</td>
                               <td>` +ativovalido+ `</td> 
                               <td> <img src='img/`+codigobarra+`.jpg' width='50' height='50'> </td> 
-                              <td><a href='#' onclick='buscar(`+codigobarra+`)'> Editar </a></td>
+                              <td><a href='#' onclick='buscar("`+nome+`", `+codigobarra+`, "`+fabricante+`", "`+categoria+`", "`+tipoprod+`", `+preco+`, `+estoque+`, `+peso+`, "`+desc+`", "`+dataform+`", `+ativo+`)'> Editar </a></td>
                               <td><a href='#' onclick='desativar(`+codigobarra+`)'> Desativar </a> </td>`;
                           html+= "</tr>";
                         }
@@ -68,41 +73,39 @@
             document.getElementById("detalhes").innerHTML = `<form action='desativar.php' method='POST' enctype='multipart/form-data'>
           <label>
           <span class='cbarras'>Codigo de barras</span>
-          <input type='text' name='cbarras' value='`+e+`'>
+          <input type='text' name='cbarras' value='`+e+`'>  
           </label>
           <label>
             <span>Desativar produto:</span>
-            <input type="checkbox" id='situacaoprod' name='situacao' value='ativo'>
             </label>
-            <button type='submit' name='acao'>Confirmar</button>     
+            <button type='submit' name='acao' onclick="alert('Desativado com sucesso')">Confirmar</button>     
         </form>`
           }
-          function buscar(e){
-            
+          function buscar(name, cbarras, fabricante, categoria, tipoprod, preco, estoque, peso, desc, dataform, ativo){
             document.getElementById("detalhes").innerHTML = `<form action='editar.php' method='POST' enctype='multipart/form-data'>
           <label>
             <span class='pname'>Nome do produto</span>
-            <input type='text' name='pname' >
+            <input type='text' name='pname' value='`+name+`'>
           </label>
           <label>
             <span class='cbarras'>Codigo de barras</span>
-            <input type='text' name='cbarras' value='`+e+`'>
+            <input type='text' name='cbarras' value='`+cbarras+`'>
           </label>
           <label>
             <span>Fabricante</span>
-            <input type='text' name='fabricante'>
+            <input type='text' name='fabricante' value='`+fabricante+`'>
           </label>
           <label>
             <span>Categoria</span>
-            <select name='categoria'>
-              <option value='select'>Selecione a categoria</option>
+            <select name='categoria' >
+              <option value='select'>`+categoria+`</option>
               <option value='AFG'>Afghanistan</option>
             </select>
           </label>
           <label>
             <span>Tipo de produto</span>
             <select name='tipoprod'>
-              <option value='select'>Selecione a categoria</option>
+              <option value='select'>`+tipoprod+`</option>
               <option value='AFG'>Afghanistan</option>
               <option value='ALA'>Åland Islands</option>
               <option value='ALB'>Albania</option>
@@ -110,31 +113,31 @@
           </label>
           <label>
             <span>Preço de venda</span>
-            <input type='text' name='precovenda' step='any' > 
+            <input type='text' name='precovenda' step='any' value='`+preco+`'> 
           </label>
           <label>
             <span>Quantidade</span>
-            <input type='number' name='qntdprod'> 
+            <input type='number' name='qntdprod' value='`+estoque+`'> 
           </label>
           <label>
             <span>Peso (gramas)</span>
-            <input type='text' name='pesog'> 
+            <input type='text' name='pesog' value='`+peso+`'> 
           </label>
           <label>
             <span>Descrição</span>
-            <input type='text' name='desc'> 
+            <input type='text' name='desc' value='`+desc+`'> 
           </label>
           <label>
          
-            <span>Upload da imagem</span>
+            <span>Upload da imagem (obrigatório)</span>
               <input type='file' name='file'/>
           </label>
           <label>
             <span>Data da inclusão</span>
-            <input type='date' name='indata'> 
+            <input type='date' name='indata' value='`+dataform+`'> 
           </label>
           <label>
-              <span>Ativar produto:</span>
+              <span>Ativar produto: (obrigatório)</span>
               <input type="checkbox" id='situacaoprod' name='situacao' value='ativo'>
             </label>
             <button type='submit' name='acao'>Editar</button>     
--- listar.php
+++ listar.php
@@ -15,7 +15,7 @@
             xhr.send();
             xhr.onreadystatechange = function(){
                 if (this.readyState = 4 && this.status == 200){
-                    var data = JSON.parse(this.responseText);
+                    var data = JSON.parse(this?.responseText || '{}');
                     var html ="";
                     var html2 ="";
 
